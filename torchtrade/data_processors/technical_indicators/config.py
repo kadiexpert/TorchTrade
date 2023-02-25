81a6df97a871304d0823163e017e00454dc0dd98
kadiexpert <kadiexpert@gmail.com>
2023-02-25 12:39:24 +0100

    Config file for technical indicators

---
+++ torchtrade/data_processors/technical_indicators/config.py
@@ -0,0 +1,23 @@
+indicators_config = {
+    'atr': {
+        'timeperiod': 24,
+    },
+    'sma': {
+        'timeperiod': 20,
+        'price': 'close'
+    },
+    'ema': {
+        'timeperiod': 50,
+        'price': 'close'
+    },
+    'rsi': {
+        'timeperiod': 14,
+        'price': 'close'
+    },
+    'macd': {
+        'fastperiod': 12,
+        'slowperiod': 26,
+        'signalperiod': 9,
+        'price': 'close'
+    }
+}
