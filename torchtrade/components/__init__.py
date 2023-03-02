e70297a07a01b35611772d84a567cf20a9bab5e7
kadiexpert <kadiexpert@gmail.com>
2023-03-02 14:55:29 +0100

    Removing Clock

--- torchtrade/components/__init__.py
+++ torchtrade/components/__init__.py
@@ -1,4 +1,3 @@
 from torchtrade.components.market import Market
 from torchtrade.components.trade import Trade,TradeDirection,TradeStatus
-from torchtrade.components.clock import Clock
 from torchtrade.components.broker import Broker
